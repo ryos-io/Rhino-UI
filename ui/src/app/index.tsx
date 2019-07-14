fe5770e2ab8f935ddfd7ebb103ded287e9f5b151
Mustafa Caylak <mustafa.caylak@web.de>
2019-07-14 10:54:26 +0200

    playing aroudn with functional components

--- ui/src/app/index.tsx
+++ ui/src/app/index.tsx
@@ -2,40 +2,29 @@ import * as React from 'react'
 import { Container } from 'react-bootstrap'
 import { BrowserRouter, Route } from 'react-router-dom'
 import { Navigation } from '../navigation'
-import { Props, State } from './model'
+import { Props } from './model'
 import Routes from '../routes'
 import { ProtectedRouteProps } from 'routes/ProtectedRoute'
 
-const initialState = Object.freeze({
-    isAuthenticated: true,
-})
-const defaultProps = Object.freeze({})
-
-export default class App extends React.Component<Props, State> {
-    static readonly defaultProps = defaultProps
-    readonly state = initialState
-
-    userHasAuthenticated = (authenticated: boolean) => {
-        this.setState({ isAuthenticated: authenticated })
+export const App: React.FC<Props> = () => {
+    const [isAuthenticated, userHasAuthenticated] = React.useState(false)
+    const defaultProtectedRouteProps: ProtectedRouteProps = {
+        isAuthenticated,
+        userHasAuthenticated,
+        authenticationPath: '/login',
     }
-
-    render = () => {
-        const defaultProtectedRouteProps: ProtectedRouteProps = {
-            isAuthenticated: this.state.isAuthenticated,
-            authenticationPath: '/login',
-        }
-
-        return (
-            <BrowserRouter>
-                <header>
-                    <Route component={Navigation} />
-                </header>
-                <main>
-                    <Container fluid>
-                        <Routes {...defaultProtectedRouteProps} />
-                    </Container>
-                </main>
-            </BrowserRouter>
-        )
-    }
+    return (
+        <BrowserRouter>
+            <header>
+                <Route component={Navigation} />
+            </header>
+            <main>
+                <Container fluid>
+                    <Routes {...defaultProtectedRouteProps} />
+                </Container>
+            </main>
+        </BrowserRouter>
+    )
 }
+
+export default App
--- ui/src/routes/ProtectedRoute.tsx
+++ ui/src/routes/ProtectedRoute.tsx
@@ -4,21 +4,20 @@ import { Redirect, Route, RouteProps } from 'react-router-dom'
 
 export interface ProtectedRouteProps extends RouteProps {
     isAuthenticated: boolean
-    authenticationPath: string
+    authenticationPath: string,
+    userHasAuthenticated: (isAuthenticated: boolean) => void
 }
 
-export class ProtectedRoute extends Route<ProtectedRouteProps> {
-    public render() {
-        let redirectPath: string = ''
-        if (!this.props.isAuthenticated) {
-            redirectPath = this.props.authenticationPath
-        }
+export const ProtectedRoute: React.FC<ProtectedRouteProps> = (props) => {
+    let redirectPath: string = ''
+    if (!props.isAuthenticated) {
+        redirectPath = props.authenticationPath
+    }
 
-        if (redirectPath) {
-            const renderComponent = () => <Redirect to={{ pathname: redirectPath }} />
-            return <Route {...this.props} component={renderComponent} render={undefined} />
-        } else {
-            return <Route {...this.props} />
-        }
+    if (redirectPath) {
+        const renderComponent = () => <Redirect to={{ pathname: redirectPath }} />
+        return <Route {...props} component={renderComponent} render={undefined} />
+    } else {
+        return <Route {...props} />
     }
 }
